8c7fa3ea1be49054394b4fa3c7f59c547c22ec13
Jacob Okoshi <jacob.okoshi@mymail.champlain.edu>
2021-03-04 07:33:44 -0500

    completed assignment

--- src/sort.h
+++ src/sort.h
@@ -30,7 +30,7 @@
 
 #ifndef sort_hpp
 #define sort_hpp
-
+#include<iostream>
 #include <algorithm> // for swap(), merge()
 #include <random>
 
@@ -48,6 +48,23 @@ namespace csi281 {
     template <typename T>
     void mergeSort(T array[], const int start, const int end) {
         // YOUR CODE HERE
+        /* pseudocode from https://www.geeksforgeeks.org/merge-sort/
+        MergeSort(arr[], l,  r)
+        If r > l
+     1. Find the middle point to divide the array into two halves:  
+             middle m = l+ (r-l)/2
+     2. Call mergeSort for first half:   
+             Call mergeSort(arr, l, m)
+     3. Call mergeSort for second half:
+             Call mergeSort(arr, m+1, r)
+     4. Merge the two halves sorted in step 2 and 3:
+             Call merge(arr, l, m, r)*/
+        if (start >= end)
+            return;      
+        int split = start + (end-start) / 2;//midpoint of start and end
+        mergeSort(array, start, split);//merge sorts first half, works recursively
+        mergeSort(array, split + 1, end);//merge sorts second half, works recursively
+        inplace_merge(array+start, array+(split+1), array+(end+1)); //merges the two returned sorted halfs
     }
     
     // setup random number generator
@@ -70,6 +87,37 @@ namespace csi281 {
     template <typename T>
     void quickSort(T array[], const int start, const int end) {
         // YOUR CODE HERE
+        /* pseudocode from https://www.geeksforgeeks.org/quick-sort/
+         low  --> Starting index,  high  --> Ending index 
+        quickSort(arr[], low, high)
+        {
+            if (low < high)
+            {
+                 pi is partitioning index, arr[pi] is now
+                   at right place 
+                pi = partition(arr, low, high);
+
+                quickSort(arr, low, pi - 1);  // Before pi
+                quickSort(arr, pi + 1, high); // After pi
+            }
+        }
+        */
+     //a Quicksort Algorithm
+        if (start >= end)//returns the fully sorted algorithm up through the iterations.
+            return;
+        T baseLine = array[end];//sets anchor to the right extreme
+        int pivot = start;//sets the "split" between elements greater than and less than the anchor to left extreme
+        for (int x = start; x <= end; x++)
+        {
+            if (array[x] <= baseLine) {
+                //checks if current index is greater than or less than the anchor, if its less it will be sent to the left of the split.
+                //swap method
+                swap(array[x], array[pivot]);
+                pivot++;
+            }
+        }
+        quickSort(array, start, pivot - 2);//recursive call to the left of the split
+        quickSort(array, pivot, end);//recursive call to the right of the split
     }
     
     // Performs an in-place ascending sort of *array*
@@ -87,7 +135,20 @@ namespace csi281 {
     template <typename T>
     void insertionSort(T array[], const int start, const int end) {
         // YOUR CODE HERE
+        for (int x = start+1; x < end+1; x++) {
+            T targetVal = array[x]; //value to be compared against, initialized at index x
+            //checks every value left of x
+            int y = x - 1;
+            while (y >= start && array[y] > targetVal) { //if the current index is larger than targetVal, the loop breaks
+                //shifts each value to the right by one
+                array[y + 1] = array[y];
+                y--;
+            }
+            //inserts targetVal into index left of the last index shifted
+            array[y + 1] = targetVal;
+        }      
     }
+}
     
     // Performs an in-place ascending sort of *array*
     // using merge sort until there is less than 10 elements, and then
@@ -100,10 +161,19 @@ namespace csi281 {
     template <typename T>
     void hybridSort(T array[], const int start, const int end) {
         // YOUR CODE HERE
+        if (start >= end)
+        {
+            return;
+        }
+        if (end - start <= 10)//checks range between start and end, this is our unsorted section
+        {
+            insertionSort(array, start, end);
+        }
+        else {
+            int split = start + (end - start) / 2;//midpoint of start and end
+            hybridSort(array, start, split);//merge sorts first half, works recursively
+            hybridSort(array, split + 1, end);//merge sorts second half, works recursively
+            inplace_merge(array + start, array + (split + 1), array + (end + 1)); //merges the two returned sorted halfs
+        }
     }
-    
-    
-}
-
-
 #endif /* sort_hpp */
